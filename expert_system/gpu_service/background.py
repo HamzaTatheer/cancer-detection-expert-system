8e3882eb6d343643f7ce7b99a38dc644ca5b3697
root <hamzatatheer@gmail.com>
2022-04-13 16:37:31 +0000

    gpu service docker file

--- expert_system/gpu_service/background.py
+++ expert_system/gpu_service/background.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
-app = Celery('background',broker='pyamqp://guest@localhost//')
+#port of rabbitmq must be 5672 and url should not contain port. It will find itself
+app = Celery('background',broker='amqp://guest:@localhost')
 
 @app.task
 def add(x, y):
