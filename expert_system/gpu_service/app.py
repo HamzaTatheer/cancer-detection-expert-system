1062b31f848334fb88691e1b8362bbbb6e4b7e5b
hamzatatheer <hamzatatheer@gmail.com>
2022-04-14 10:06:28 +0000

    gpu

--- expert_system/gpu_service/app.py
+++ expert_system/gpu_service/app.py
@@ -10,6 +10,9 @@ import jsonpickle
 import cv2
 import openslide
 from io import BytesIO
+import background
+from background import runInference
+from celery.result import AsyncResult
 
 
 app = Flask(__name__)
@@ -25,13 +28,40 @@ def data_uri_to_cv2_img(uri):
    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
    return img
 
-@app.route("/runInference",methods = ['POST'])
-def runInference():
+@app.route("/getInferenceResult",methods = ['POST'])
+def getInferenceResult():
     data = request.get_json()
+    task_id = data['task_id']
+    
+    result = AsyncResult(task_id,app=background.app)
+    
+    #img = data_uri_to_cv2_img(img_str)
+    #cv2.imwrite('test.png',img)
+
+    isReady = result.ready()
+    print("Result: "+str(isReady))
+    ans = None
+    if (isReady):
+        ans = result.get(5)
+
+    if(ans == None):
+        return "Result Still Not Exists"
+
+
+    return ans
+
+@app.route("/startInference",methods = ['POST'])
+def startInference():
+    #print(request.data)
+    data = request.get_json()
+    img_name = data['img_name']
     img_str = data['img_str']
-    img = data_uri_to_cv2_img(img_str)
-    cv2.imwrite('test.png',img)
-    return "Goodd"
+    #img = data_uri_to_cv2_img(img_str)
+    job = runInference.delay(img_name,img_str)
+    id = job.task_id
+    return id
+
+
 
 
 if __name__ == "__main__":
